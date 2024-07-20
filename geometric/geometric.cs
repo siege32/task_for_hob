c8960d52342d85501922151acc5a74993bb67a93
siege32 <boss.gurkalo@mail.ru>
2024-07-20 23:51:56 +0700

    release

---
+++ geometric/geometric.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometric
+{
+    public static class area
+    {
+        public const double PI = 3.1415926535897931;
+
+        public static double circle_area(double radius)
+        {
+            return PI * Math.Pow(radius, 2);
+        }
+
+        public static double triangle_area(double first_side, double second_side, double third_side)
+        {
+            double[] sides = { first_side, second_side, third_side };
+
+            double leg_first = sides.Min();
+            double hypotinuse = sides.Max();
+            double leg_second = sides.Sum() - leg_first - hypotinuse;
+
+            if (Math.Pow(leg_first, 2) + Math.Pow(leg_second, 2) == Math.Pow(hypotinuse, 2))
+            {
+                return (leg_first * leg_second) / 2;
+            }
+            
+            else
+            {
+                double perimeter = (sides[0] + sides[1] + sides[2]) / 2;
+
+                return Math.Sqrt(perimeter * (perimeter - sides[0]) * (perimeter - sides[1]) * (perimeter - sides[2]));
+            }
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using geometric;
+
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double circle = area.circle_area(4);
+            double rectangular_triangle = area.triangle_area(3, 4, 5);
+            double triangle = area.triangle_area(6.7, 7.9, 10.7);
+            Console.WriteLine(circle);
+            Console.WriteLine(rectangular_triangle);
+            Console.WriteLine(triangle);
+            Console.ReadLine();
+        }
+    }
+}
